79eb52392ef843559b6143a96e4ea18bc1e0deb5
SOHAM DUTTA <sohamdutta3762@gmail.com>
2026-09-18 23:19:31 +0530

    Avoid hydration warning for persisted theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ const themeScript = `
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${display.variable} ${sans.variable} ${mono.variable}`}>
+    <html lang="en" suppressHydrationWarning className={`${display.variable} ${sans.variable} ${mono.variable}`}>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
